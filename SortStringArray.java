f5d0bd1d0efda65c7228ff355c5e49f2cb67c7bc
Tierje13 <tierje11@gmail.com>
2016-11-17 11:50:49 -0700

    Final

---
+++ SortStringArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SortStringArray {
+
+	public static void main(String[] args) {
+		String ss[]= {"Bill", "Mary", "Lee", "Agnes", "Alfred", "Thomas", "Alvin", "Bernard", "Ezra","Herman"};
+		Arrays.sort(ss);
+		for (int j=0; j<10; j++){
+			System.out.println(ss[j]);
+			
+			
+			}
+	
+		}
+
+	}
+
+
